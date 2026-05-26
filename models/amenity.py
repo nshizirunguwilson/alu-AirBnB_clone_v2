2c10cd6f6cb004142cc54a3fa84d2c24e565d885
Wilson Nshizirungu <w.nshizirun@alustudent.com>
2026-05-26 13:55:47 +0200

    Map Amenity and Review models to their tables

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+""" Amenity Module for HBNB project """
+from sqlalchemy import Column, String
+from models.base_model import BaseModel, Base
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """ Amenity class """
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
